26d65061a50b6e833c3ab3fe81d877a860030f95
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2025-08-15 08:01:31 -0500

    search box is now functional

--- frontend/src/app/main/page.tsx
+++ frontend/src/app/main/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
@@ -12,7 +12,8 @@ import Button from "@mui/material/Button";
 import { backendUrl } from "./config";
 
 export default function TitlebarImageList() {
-  const [itemData, setItemData] = React.useState([]);
+  const [itemData, setItemData] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     console.log("starting fetch");
@@ -25,6 +26,10 @@ export default function TitlebarImageList() {
       .catch((error) => console.error("Error:", error));
   }, []);
 
+  useEffect(() => {
+    console.log("searchText", searchText);
+  }, [searchText]);
+
   return (
     <div className="main" style={styles.main}>
       <div className="container" style={styles.container}>
@@ -34,6 +39,7 @@ export default function TitlebarImageList() {
             label="Search images"
             type="search"
             variant="filled"
+            onChange={(e) => setSearchText(e.target.value)}
           />
           <Button variant="contained">Upload</Button>
         </div>
@@ -42,108 +48,39 @@ export default function TitlebarImageList() {
             <ImageListItem key="Subheader" cols={2}>
               <ListSubheader component="div">December</ListSubheader>
             </ImageListItem>
-            {itemData.map((item) => (
-              <ImageListItem key={item.img}>
-                <img
-                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  src={`${item.img}?w=248&fit=crop&auto=format`}
-                  alt={item.title}
-                  loading="lazy"
-                />
-                <ImageListItemBar
-                  title={item.title}
-                  subtitle={item.author}
-                  actionIcon={
-                    <IconButton
-                      sx={{ color: "rgba(255, 255, 255, 0.54)" }}
-                      aria-label={`info about ${item.title}`}
-                    >
-                      <InfoIcon />
-                    </IconButton>
-                  }
-                />
-              </ImageListItem>
-            ))}
+            {itemData.filter((item) => {
+              if (searchText === "") return true;
+              // filter by input text, ignore case. Note: g isn't necessary since filter handels iteration.
+              const re = new RegExp(searchText, "gi")
+              return item.title.match(re)
+            }).map((item) => (
+                <ImageListItem key={item.img}>
+                  <img
+                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    alt={item.title}
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={item.title}
+                    subtitle={item.author}
+                    actionIcon={
+                      <IconButton
+                        sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                        aria-label={`info about ${item.title}`}
+                      >
+                        <InfoIcon />
+                      </IconButton>
+                    }
+                  />
+                </ImageListItem>
+              ))}
           </ImageList>
         </div>
       </div>
     </div>
   );
 }
-
-// const itemData = [
-//   {
-//     img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-//     title: "Breakfast",
-//     author: "@bkristastucchio",
-//     rows: 2,
-//     cols: 2,
-//     featured: true,
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-//     title: "Burger",
-//     author: "@rollelflex_graphy726",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-//     title: "Camera",
-//     author: "@helloimnik",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-//     title: "Coffee",
-//     author: "@nolanissac",
-//     cols: 2,
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-//     title: "Hats",
-//     author: "@hjrc33",
-//     cols: 2,
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
-//     title: "Honey",
-//     author: "@arwinneil",
-//     rows: 2,
-//     cols: 2,
-//     featured: true,
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
-//     title: "Basketball",
-//     author: "@tjdragotta",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
-//     title: "Fern",
-//     author: "@katie_wasserman",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
-//     title: "Mushrooms",
-//     author: "@silverdalex",
-//     rows: 2,
-//     cols: 2,
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
-//     title: "Tomato basil",
-//     author: "@shelleypauls",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
-//     title: "Sea star",
-//     author: "@peterlaster",
-//   },
-//   {
-//     img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
-//     title: "Bike",
-//     author: "@southside_customs",
-//     cols: 2,
-//   },
-// ];
 
 const appWidth = "70vw";
 
